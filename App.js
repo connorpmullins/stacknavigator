5a778be8b57ad7bac7bb66f5a1b8795766a31e9b
connorpmullins <changeandadapt@gmail.com>
2017-11-16 08:45:33 -0800

    first commit
    git commit -m first

---
+++ App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+
+const HomeScreen = ({ navigation }) => (
+  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Text>Home Screen</Text>
+    <Button
+      onPress={() => navigation.navigate('Details')}
+      title="Go to details"
+    />
+  </View>
+  );
+
+const DetailsScreen = ({ navigation }) => (
+  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Text>Details Screen</Text>
+    <Button
+      onPress={() => navigation.navigate('Home')}
+      title="Go to Home Screen"
+    />
+  </View>
+);
+
+const RootNavigator = StackNavigator({
+  Home: {
+    screen: HomeScreen,
+    navigationOptions: {
+      headerTitle: 'Home',
+    },
+  },
+  Details: {
+    screen: DetailsScreen,
+    navigationOptions: {
+      headerTitle: 'Details',
+    },
+  },
+});
+
+export default RootNavigator;
